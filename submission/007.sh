e0510e7bfde91c1bb34b1820c48ae54a8ee21306
Lucas Balieiro <lucasbalieirom@hotmail.com>
2024-11-20 01:44:18 -0400

    exercises1 to 7

--- submission/007.sh
+++ submission/007.sh
@@ -1,1 +1,14 @@
 # Only one single output remains unspent from block 123,321. What address was it sent to?
+#https://developer.bitcoin.org/reference/rpc/gettxout.html
+#https://bitcoin.stackexchange.com/questions/48504/how-to-check-if-an-output-has-been-spent
+block_hash=$(bitcoin-cli getblockhash 123321)
+utxo_array=$(bitcoin-cli getblock $block_hash 2 | jq -r '.tx[] |.txid as $txid |.vout[] |"\($txid),\(.n),\(.scriptPubKey.address)"')
+
+while IFS=',' read -r txid vout address; do
+    gettxout_output=$(bitcoin-cli gettxout "$txid" "$vout")
+    
+    if [[ -n $gettxout_output ]]; then
+        echo $address
+        break
+    fi
+done <<< "$utxo_array"
